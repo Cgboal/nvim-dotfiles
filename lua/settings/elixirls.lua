876c568e0128462175d690680d05f2b48b17d432
Cgboal <cgboal521@gmail.com>
2023-04-27 01:38:23 +0100

    Initial Commit

---
+++ lua/settings/elixirls.lua
@@ -0,0 +1,3 @@
+return {
+  cmd = { "language_server.sh"}
+}
---
+++ lua/user/colorscheme.lua
@@ -0,0 +1,15 @@
+local M = {
+  "rebelot/kanagawa.nvim",
+  lazy = false,
+  priority = 1000
+}
+
+M.name = "kanagawa"
+function M.config()
+  local status_ok, _ = pcall(vim.cmd.colorscheme, M.name)
+  if not status_ok then
+    return
+  end
+end
+
+return M
